2d6ad33ec440df6d377f75977cad24943c5aa146
kkuratolodon <mif05123@gmail.com>
2024-12-05 19:45:20 +0700

    konz

--- spongebob/settings.py
+++ spongebob/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p6_rz^%ddks&z0xm0a8iow-ymu790a=j4&*gqqg)a($)d-kkmr
 
 # SECURITY WARNING: don't run with debug turned on in production!
 PRODUCTION = os.getenv("PRODUCTION", False)
-DEBUG = not PRODUCTION
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost","127.0.0.1","muhammad-irfan25-kage.pbp.cs.ui.ac.id"]
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://muhammad-irfan25-kage.pbp.cs.ui.ac.id", "https://muhammad-irfan25-kage.pbp.cs.ui.ac.id"]
